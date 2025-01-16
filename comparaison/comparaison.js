be65867c3de37830601014d01b3f51a77f60a3ac
Lesu <122235588+Lesuu@users.noreply.github.com>
2025-01-16 18:19:31 +0100

    Update texte + chargement
    - Fonction async pour s'assurer que les données soient chargées avant de tourner le reste du code
    - Randomisé la positions des options de réponses
    - Update le texte selon la recommendation de Lina
    - Ajouté un maximum de 10 questions
    - Cliquer n'importe où sur l'écran permet de passer à la prochaine question

--- comparaison/comparaison.js
+++ comparaison/comparaison.js
@@ -19,7 +19,7 @@ let questions_JV = []
 let questions_JdS = []
 async function loadData() {
     CSVdata = await getCSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vQWBSQtcLt8CbTPN-TvHnrCt1h24GtoXiWxBCoo3nqbrTSqLuc93FeogkFsOrfS_qF-YDyhTk5E0aau/pub?output=csv');
-
+    console.log(CSVdata)
     for (let i = 0; i < CSVdata.length; i++) {
         if (CSVdata[i].jeu_video == "TRUE") {
             questions_JV.push(CSVdata[i]) 
@@ -28,8 +28,6 @@ async function loadData() {
         }
     }
 }
-
-loadData()
 
 // Initialisation de Kaplay
 kaplay({
@@ -40,251 +38,284 @@ kaplay({
     stretch:true,
 })
 
-//#region Asset loading
+main()
+async function main() {
+    // On attend que les données soient chargées pour lancer le programme
+    await loadData()
 
-loadFont("pixel", "assets/fonts/PixelOperator8-Bold.ttf")
+    //#region Asset loading
 
-loadSprite("card", "assets/sprites/card.png")
-loadSprite("jv_icon", "assets/sprites/video_game.png")
-loadSprite("jv_color", "assets/sprites/vg_color.png")
-loadSprite("jds_icon", "assets/sprites/board_game.png")
-loadSprite("jds_color", "assets/sprites/bg_color.png")
+    loadFont("pixel", "assets/fonts/PixelOperator8-Bold.ttf")
+
+    loadSprite("card", "assets/sprites/card.png")
+    loadSprite("jv_icon", "assets/sprites/video_game.png")
+    loadSprite("jv_color", "assets/sprites/vg_color.png")
+    loadSprite("jds_icon", "assets/sprites/board_game.png")
+    loadSprite("jds_color", "assets/sprites/bg_color.png")
 
 
-const scaleValue = (width()/height())*1.3;
+    const scaleValue = (width()/height())*1.3;
 
-// Variables globales
-let jv 
-let question_number = 0
-let clicked = 0
-let categorie
-let score = 0
+    //#region Variables globales
+    let jv 
+    let question_number = 0
+    let clicked = 0
+    let categorie
+    let score = 0
+    let compteur_question 
 
-//#region Ecran d'accueil
+    // #region Constantes
+    const nbr_questions = 10
 
-scene("titleScreen", () => {
-    // Réinitialisation du score
-    score = 0
+    //#region Ecran d'accueil
 
-    // Elements UI
-    let title = add([
-        text("Choisi une option !", {
-            font: "pixel",
-            size: 16
-        }),
-        pos(width()/2 , height()/5),
-        scale(scaleValue),
-        anchor("center")
-    ])
-    let jv_icon = add([
-        sprite("jv_icon"),
-        pos(width()/2.5, height()/2),
-        scale(scaleValue/5),  
-        anchor("right"),
-        area(),
-        "jv_icon"
-    ])
-    let jds_icon = add([
-        sprite("jds_icon"),
-        pos(width()/1.8, height()/2),
-        scale(scaleValue/5),  
-        anchor("left"),
-        area(),
-        "jds_icon"
-    ])
-    // Changement de la texture quand on survole les icones...
-    jv_icon.onHover(() => jv_icon.sprite = "jv_color")
-    jds_icon.onHover(() => jds_icon.sprite = "jds_color")
+    scene("titleScreen", async () => {
+        // Réinitialisation du score
+        score = 0
+        compteur_question = 0
 
-    // ... et quand on lâche
-    jv_icon.onHoverEnd(() => jv_icon.sprite = "jv_icon")
-    jds_icon.onHoverEnd(() => jds_icon.sprite = "jds_icon")
-
-    // Changement de scène, établit si on a cliqué sur jv ou jds grâce a la variable jv
-    jv_icon.onClick(() => {
-        jv = true
-        categorie = [...questions_JV]
-        go("questions")
-    })
-    jds_icon.onClick(() => {
-        jv = false
-        categorie = [...questions_JdS]
-        go("questions")
-    })
-})
-
-// Scène ou on pose les questions
-scene("questions", () => {
-    // Couleur du background dépend du support choisi
-    if (jv){
-        setBackground(120,0,30)
-    } else {
-        setBackground(0,100,0)
-    }
-  
-    // Randomiser la position des cartes 
-    let x_card1 = width() / (randi() + 1.5);
-    let x_card2 = width() / (x_card1 === width() / 1.5 ? 2.5 : 1.5);
-
-    // Choisi une question aléatoire
-    question_number = Math.floor(rand(categorie.length))
-    let caption = add([
-        text("Quelle option a le moins d'impact sur le changement climatique ?", {
-            font: "pixel",
-            size: 32
-        }),
-        pos(width()/2, height()/6),
-        anchor("center"),
-        "question_element"
-    ])
-    let card1 = add([
-        sprite("card"),
-        pos(x_card1, height()/2),
-        scale(scaleValue/1.2),  
-        anchor("center"),
-        area(),
-        "question_element"
-    ])
-    console.log(categorie)
-    let text1 = add([
-        text(categorie[question_number].description_activite1, {
-            font: "pixel",
-            size: 24,
-            width: 250,
-            align: "center"
-        }),
-        color(56, 71, 74),
-        pos(card1.pos),
-        anchor("center"),
-        z("100"),
-        "question_element"
-    ])
-    let card2 = add([
-        sprite("card"),
-        pos(x_card2, height()/2),
-        scale(scaleValue/1.2),  
-        anchor("center"),
-        area(),
-        "question_element"
-    ])
-    let text2 = add([
-        text(categorie[question_number].description_activite2, {
-            font: "pixel",
-            size: 22,
-            width: 250,
-            align: "center"
-        }),
-        color(56, 71, 74),
-        pos(card2.pos),
-        anchor("center"),
-        z("100"),
-        "question_element"
-    ])
-    card1.onClick(() => {
-        clicked = 1
-        go("results")
-    })
-    card2.onClick(() => {
-        clicked = 2
-        go("results")
-    })
-})
-    
-    
-let caption_result
-scene("results", () =>{
-    if (((categorie[question_number].activite1_gagnante == "TRUE") && (clicked == 1)) || 
-            (categorie[question_number].activite1_gagnante == "FALSE") && (clicked == 2)){
-        caption_result = "C'est correct!"
-        score += 100
-    } else {
-        caption_result = "C'est faux."
-    }
-    let result = add([
-        text(caption_result, {
-            font: "pixel",
-            size: 32,
-            width: 500,
-            align: "center"
-        }),
-        pos(width()/2, height()/6),
-        anchor("center"),
-        "results_element"
-    ])
-    let commentaire = add([
-        text(categorie[question_number].commentaire, {
-            font: "pixel",
-            size: 32,
-            width: 800,
-            align: "center"
-        }),
-        pos(width()/2, height()/2),
-        anchor("center"),
-        "results_element"
-    ])
-    
-    if (categorie.length - 1 > 0){ 
-        let suivant = add([
-            text("Prochaine question",{
+        // Elements UI
+        let title = add([
+            text("Choisi une option !", {
                 font: "pixel",
-                size: 32,
-                align: "center"
+                size: 16
             }),
-            pos(width()/2, height()/1.2),
-            anchor("center"),
-            area(),
-            "results_element"
+            pos(width()/2 , height()/5),
+            scale(scaleValue),
+            anchor("center")
         ])
-        
-        suivant.onClick(() => {
-            console.log(categorie.length)
-            categorie.splice(question_number, 1)
+        let jv_icon = add([
+            sprite("jv_icon"),
+            pos(width()/2.5, height()/2),
+            scale(scaleValue/5),  
+            anchor("right"),
+            area(),
+            "jv_icon"
+        ])
+        let jds_icon = add([
+            sprite("jds_icon"),
+            pos(width()/1.8, height()/2),
+            scale(scaleValue/5),  
+            anchor("left"),
+            area(),
+            "jds_icon"
+        ])
+        // Changement de la texture quand on survole les icones...
+        jv_icon.onHover(() => jv_icon.sprite = "jv_color")
+        jds_icon.onHover(() => jds_icon.sprite = "jds_color")
+
+        // ... et quand on lâche
+        jv_icon.onHoverEnd(() => jv_icon.sprite = "jv_icon")
+        jds_icon.onHoverEnd(() => jds_icon.sprite = "jds_icon")
+
+        // Changement de scène, établit si on a cliqué sur jv ou jds grâce a la variable jv
+        await jv_icon.onClick(() => {
+            jv = true
+            categorie = [...questions_JV]
             go("questions")
         })
-    } else {
-        let fin = add([
-            text("Résultats finaux",{
+        await jds_icon.onClick(() => {
+            jv = false
+            categorie = [...questions_JdS]
+            go("questions")
+        })
+    })
+
+    // #region Questions
+    // Scène ou on pose les questions
+    scene("questions", () => {
+        // Couleur du background dépend du support choisi
+        if (jv){
+            setBackground(120,0,30)
+        } else {
+            setBackground(0,100,0)
+        }
+    
+        // Randomiser la position des cartes 
+        let x_card1 = width() / (randi() + 1.5);
+        let x_card2 = width() / (x_card1 === width() / 1.5 ? 2.5 : 1.5);
+
+        // Compteur de question
+
+        compteur_question ++
+        let compteur_caption = add([
+            text(`Question ${compteur_question}/${nbr_questions}`, {
+                font: "pixel",
+                size: 16
+            }),
+            pos(width() - width()/1.1, height()*0.04),
+            anchor("center"),
+            "question_element"
+        ])
+
+
+        // Choisi une question aléatoire
+        question_number = Math.floor(rand(categorie.length))
+        console.log(question_number)
+        let caption = add([
+            text("Quelle option a le moins d'impact sur le changement climatique ?", {
+                font: "pixel",
+                size: 32,
+                width: 1000,
+                align: "center"
+            }),
+            pos(width()/2, height()/6),
+            anchor("center"),
+            "question_element"
+        ])
+        let card1 = add([
+            sprite("card"),
+            pos(x_card1, height()/2),
+            scale(scaleValue/1.2),  
+            anchor("center"),
+            area(),
+            "question_element"
+        ])
+        console.log(categorie)
+        let text1 = add([
+            text(categorie[question_number].description_activite1, {
+                font: "pixel",
+                size: 24,
+                width: 250,
+                align: "center"
+            }),
+            color(56, 71, 74),
+            pos(card1.pos),
+            anchor("center"),
+            z("100"),
+            "question_element"
+        ])
+        let card2 = add([
+            sprite("card"),
+            pos(x_card2, height()/2),
+            scale(scaleValue/1.2),  
+            anchor("center"),
+            area(),
+            "question_element"
+        ])
+        let text2 = add([
+            text(categorie[question_number].description_activite2, {
+                font: "pixel",
+                size: 22,
+                width: 250,
+                align: "center"
+            }),
+            color(56, 71, 74),
+            pos(card2.pos),
+            anchor("center"),
+            z("100"),
+            "question_element"
+        ])
+        card1.onClick(() => {
+            clicked = 1
+            go("results")
+        })
+        card2.onClick(() => {
+            clicked = 2
+            go("results")
+        })
+    })
+        
+        
+    //#region Réponse question
+    // Scène qui affiche la réponse à la question
+    let caption_result
+    scene("results", () =>{
+        if (((categorie[question_number].activite1_gagnante == "TRUE") && (clicked == 1)) || 
+                (categorie[question_number].activite1_gagnante == "FALSE") && (clicked == 2)){
+            caption_result = "C'est correct!"
+            score += 100
+        } else {
+            caption_result = "C'est faux."
+        }
+        let result = add([
+            text(caption_result, {
+                font: "pixel",
+                size: 32,
+                width: 500,
+                align: "center"
+            }),
+            pos(width()/2, height()/6),
+            anchor("center"),
+            "results_element"
+        ])
+        let commentaire = add([
+            text(categorie[question_number].commentaire, {
+                font: "pixel",
+                size: 32,
+                width: 800,
+                align: "center"
+            }),
+            pos(width()/2, height()/2),
+            anchor("center"),
+            "results_element"
+        ])
+        
+        if ((categorie.length - 1 > 0) && (compteur_question < nbr_questions)){ 
+            let suivant = add([
+                text("Prochaine question",{
+                    font: "pixel",
+                    size: 32,
+                    align: "center"
+                }),
+                pos(width()/2, height()/1.2),
+                anchor("center"),
+                area(),
+                "results_element"
+            ])
+            
+            onClick(() => {
+                console.log(categorie.length)
+                categorie.splice(question_number, 1)
+                go("questions")
+            })
+        } else {
+            let fin = add([
+                text("Résultats finaux",{
+                    font: "pixel",
+                    size: 32,
+                    align: "center"
+                }),
+                pos(width()/2, height()/1.2),
+                anchor("center"),
+                area(),
+                "results_element"
+            ])
+            
+            onClick(() => {
+                go("finalResults", {score: score})
+            })
+        }
+    })
+
+    //#region Résultats finaux
+    scene("finalResults", ({score}) =>{
+        let scoreLabel = add([
+            text(`Bravo! Tu as réalisé un score de ${score}.`,{
+                font: "pixel",
+                size: 32,
+                align: "center"
+            }),
+            pos(width()/2, height()/6),
+            anchor("center"),
+
+        ])
+        let quit = add([
+            text("Terminer",{
                 font: "pixel",
                 size: 32,
                 align: "center"
             }),
             pos(width()/2, height()/1.2),
             anchor("center"),
-            area(),
-            "results_element"
+            area()
         ])
-        
-        fin.onClick(() => {
-            go("finalResults", {score: score})
+        onClick(()=>{
+            go("titleScreen")
         })
-    }
-})
-
-scene("finalResults", ({score}) =>{
-    let scoreLabel = add([
-        text(`Bravo! Tu as réalisé un score de ${score}.`,{
-            font: "pixel",
-            size: 32,
-            align: "center"
-        }),
-        pos(width()/2, height()/6),
-        anchor("center"),
-
-    ])
-    let quit = add([
-        text("Terminer",{
-            font: "pixel",
-            size: 32,
-            align: "center"
-        }),
-        pos(width()/2, height()/1.2),
-        anchor("center"),
-        area()
-    ])
-    quit.onClick(()=>{
-        go("titleScreen")
-
     })
-})
 
-go("titleScreen")
+    go("titleScreen")
+
+}
+
